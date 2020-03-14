e8d35170ad9d0c590348fd97190208ef1472b8f3
Szymon Wiszczuk <golota60@gmail.com>
2020-03-14 13:52:45 +0100

    lint fix

--- client/src/main/App.tsx
+++ client/src/main/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.scss';
 import LoginPage from './LoginPage';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import HomePage from './HomePage';
 
 const App = () => {
   return (
     <Router>
       <Switch>
+        <Route path="/" exact component={() => {return(<Redirect to="/login"/>);}}/>
         <Route path="/login" component={LoginPage} />
         <Route path="/home" component={HomePage} />
       </Switch>
